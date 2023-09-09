7f6dc27863930e91df1069533d811b6e15ae88a9
LAVANYA-PIDIKITI <98797256+LAVANYA-PIDIKITI@users.noreply.github.com>
2023-09-09 22:24:11 +0530

    count_of_setbits.java added

---
+++ bit manipulation/count_of_setbits.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            int cnt = ob.setBits(N);
+            System.out.println(cnt);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int setBits(int N) {
+        if(N==0){
+            return 0;
+        }
+        else{
+            return (N&1) + setBits(N>>1);
+        }
+    }
+}
